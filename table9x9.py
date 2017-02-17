1cce094c8c19074b1645ecdd23961ba616a1a39e
Icing <fangxin516@163.com>
2017-02-17 17:05:57 +0800

    九九乘法表

--- table9x9.py
+++ table9x9.py
@@ -1,17 +1,19 @@
-#coding:utf-8
-#python3.5环境
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+__author__ = 'hstking hstking@hotmail.com'
 
 class PrintTable(object):
-	'''打印九九乘法表 '''
-	def __init__(self):
-		print(u'开始打印9x9的乘法表格')
-		self.print99()
+    '''打印九九乘法表 '''
 
-	def print99(self):
-		for i in range(1,10):
-			for j in range(1,i+1):
-				print('%dX%d=%2s  ' %(j,i,i*j)),
-			print('\n')
+    def __init__(self):
+        print(u'开始打印9x9的乘法表格')
+        self.print99()
+
+    def print99(self):
+        for i in range(1, 10):
+            for j in range(1, i + 1):
+                print("%d*%d=%2d" % (i, j, i * j), end="  ")
+            print(' ')
 
 if __name__ == '__main__':
-    pt = PrintTable()
+    pt =PrintTable()
