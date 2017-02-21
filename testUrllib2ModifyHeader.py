2ae3e964e1f826435c681f5aaf350298d39b5550
Icing <fangxin516@163.com>
2017-02-21 16:54:47 +0800

    常见的user-agent，作为模块供调用

---
+++ testUrllib2ModifyHeader.py
@@ -0,0 +1,31 @@
+#coding:utf-8
+
+import urllib2
+import userAgents
+
+'''userAgents.py是个自定义的模块，位置处于当前目录下'''
+
+class urllib2ModifyHeader(object):
+    '''使用urllib2模块修改header'''
+    def __init__(self):
+        #这个是PC + IE 的User-Agent
+        PIUA = userAgents.pcUserAgent.get('IE 9.0')
+        # 这个是Mobile + UC的User-Agent
+        MUUA = userAgents.mobileUserAgent.get('UC standard')
+        #测试用的网站选择的是有道翻译
+        self.url = 'http://fanyi.youdao.com'
+
+        self.useUserAgent(PIUA,1)
+        self.useUserAgent(MUUA,2)
+
+    def useUserAgent(self,userAgents,name):
+        request = urllib2.Request(self.url)
+        request.add_header(userAgents.split(':')[0],userAgents.split(':')[1])
+        response = urllib2.urlopen(request)
+        fileName = str(name) + '.html'
+        with open(fileName,'a') as fp:
+            fp.write("%s\n\n" %userAgents)
+            fp.write(response.read())
+
+if __name__ == '__main__':
+    umh = urllib2ModifyHeader()
