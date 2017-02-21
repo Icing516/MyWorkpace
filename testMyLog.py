430d626de227882da1ba7fe5fa9695a8a848bc06
Icing <fangxin516@163.com>
2017-02-21 18:34:46 +0800

    日志模块

---
+++ testMyLog.py
@@ -0,0 +1,11 @@
+#-*- coding: utf-8 -*-
+
+from myLog import MyLog
+
+if __name__ == '__main__':
+	ml = MyLog()
+	ml.debug('I am debug message')
+	ml.info('I am info message')
+	ml.warn('I am warn message')
+	ml.error('I am error message')
+	ml.critical('I am critical message')
