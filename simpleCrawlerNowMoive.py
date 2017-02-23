e7cf6a36ddff8b9de5b5e841808b285e2eb636d6
Icing <fangxin516@163.com>
2017-02-23 14:27:08 +0800

    爬取金逸影城首页电影动态加载的信息，用的request请求，带header和data参数

--- simpleCrawlerNowMoive.py
+++ simpleCrawlerNowMoive.py
@@ -1,39 +1,41 @@
 #coding:utf-8
 
 import re
-import urllib2
 import requests
+import json
+import sys
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 class TodayMovice(object):
     '''获取金逸影城当日影视'''
     def __init__(self):
-        self.url = 'http://www.jycinema.com/frontUIWebapp/appserver/cinCinemaFilmViewService/findFilm'
+        self.url ='http://www.jycinema.com/frontUIWebapp/appserver/cinCinemaFilmViewService/findFilm'
         self.timeout =5
         self.fileName = './todayMoive.txt'
         '''内部变量定义完毕'''
+        self.head ={'Host': 'www.jycinema.com',
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
+        self.param = {
+            'params': '''{"type":"queryFilm","cityName":"深圳市","cinemaId":"","statusRE":"RELEASE","channelCode":"J0002","channelId":"3"}'''}
         self.getMoiveInfo()
-        self.headers = {'Host':'www.jycinema.com',
-                        'Connection': 'keep-alive',
-                        'Referer': 'http: // www.jycinema.com / frontUIWebapp / templates / default / index.html',
-                        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
 
-    def getMoiveInfo(self,):
-        req = urllib2.Request(self.url,self.headers)
-        response = urllib2.urlopen(req)
-        #response = requests.get(self.url,self.headers)
-        print response.read()
-        moiveList = re.findall('"film-title">(.*?)</span>',response.read())
-        print moiveList
+    def getMoiveInfo(self):
+        req = requests.post(self.url, data=self.param, headers=self.head)
+        # print req.content
+        result = req.content
+        json_result = json.loads(result)
+        film1 = json_result['data']
+
+        for film in film1:
+            print film['filmName']
+
         with open(self.fileName,'w') as fp:
-            for moive in moiveList:
-                moive = self.subStr(moive)
-                print(moive.decode('utf-8'))
-                fp.write(moive+'\n')
-
-    def subStr(self,st):
-        st = st.replace('film-title">','')
-        st = st.replace('</h3>','')
-        return st
+            for film in film1:
+                film = str(film['filmName'])
+                fp.write(film+'\n')
 
 if __name__ =="__main__":
     tm = TodayMovice()
